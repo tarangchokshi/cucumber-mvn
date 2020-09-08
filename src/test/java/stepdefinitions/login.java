01b8c0ce668c77940113de6dd8f57e2f50f93df6
tarangchokshi <tarangchokshi143@gmail.com>
2020-09-08 23:47:10 +0530

    Update login.java

--- src/test/java/stepdefinitions/login.java
+++ src/test/java/stepdefinitions/login.java
@@ -3,15 +3,27 @@ package stepdefinitions;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
 
-public class login extends Base {
+public class login {
 
-	
+	WebDriver driver;
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
 	@Given("^Open URL$")
 	public void open_URL() throws Throwable {
-		driver = setupBrowser();
+		
 		driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
 	}
 
